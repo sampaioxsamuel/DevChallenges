b905461544d9f9b281b0d06ff8dbde9acc485ec1
Samuel Nogueira <sampaioxsamuel@gmail.com>
2020-10-22 19:18:27 -0300

    build

---
+++ quote-generator/build/precache-manifest.a77ad1edac3a9265aeee4a6bd6c66d4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c30e2bd2bd2df548d98bcc0a5e2de87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "648bfb93f0a0afb823a5",
+    "url": "/static/css/2.079ed09a.chunk.css"
+  },
+  {
+    "revision": "d68f649f65153a4a917a",
+    "url": "/static/css/main.a40dfdac.chunk.css"
+  },
+  {
+    "revision": "648bfb93f0a0afb823a5",
+    "url": "/static/js/2.ecfa37d3.chunk.js"
+  },
+  {
+    "revision": "b2df6f42d3b318f0b6ad3605dbfe5698",
+    "url": "/static/js/2.ecfa37d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68f649f65153a4a917a",
+    "url": "/static/js/main.c2c2bc47.chunk.js"
+  },
+  {
+    "revision": "363947c9e88dc625d12c",
+    "url": "/static/js/runtime-main.8716e6b8.js"
+  }
+]);
